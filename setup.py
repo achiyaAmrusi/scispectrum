cd47ab85b9eff2f1b023c70ce1c173a0b1de99da
achiyaAmrosi <ahia.amrosi@mail.huji.ac.il>
2025-08-28 11:28:41 +0300

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PySpectrum',
-    version='0.1',
+    version='0.2',
     packages=['pyspectrum', 'pyspectrum.peak', 'pyspectrum.spectrum', 'pyspectrum.calibration',
               'pyspectrum.peak_fitting', 'pyspectrum.detector_parser', 'pyspectrum.peak_identification'],
     url='https://github.com/achiyaAmrusi/pySpectrum',
